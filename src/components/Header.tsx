8c86e79711c7aa8ed6df3e9f1b098b2a7919b620
sabb12 <sabb0923@gmail.com>
2024-06-09 20:58:29 +0900

    Docs: Header 검색 css  수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,33 +1,42 @@
-
 import "../styles/header.scss";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
-    return <div className="headerWrapper">
-    <div className="headerLogoContainer">
-      <a href="#">
-        <img
-          className="headerLogo"
-          src="https://picsum.photos/seed/picsum/200/300"
-          alt=""
-          style={{width: "100px", height: "50px"}}
-        />
-      </a>
-    </div>
-    <div className="headerNavigation">
-      <input type="checkbox" className="toggle-menu" />
-      <div className="hamburger"></div>
+  return (
+    <section className="headerWrapper">
+      <div className="headerLogoContainer">
+        <a href="#">
+          <img
+            className="headerLogo"
+            src="https://picsum.photos/seed/picsum/200/300"
+            alt=""
+            style={{ width: "100px", height: "50px" }}
+          />
+        </a>
+      </div>
+      <div className="headerNavigation">
+        <input type="checkbox" className="toggle-menu" />
+        <div className="hamburger"></div>
 
-      <ul className="headerMenu">
-        <li><a href="">질의응답</a></li>
-        <li>
+        <ul className="headerMenu">
           <div className="searchInput">
-            <input type="text" /><button>검색</button>
+            <input type="text" />
+            <button>
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </button>
           </div>
-        </li>
-        <li><a href="">로그인</a></li>
-        <li><a href="">회원가입</a></li>
-      </ul>
-    </div>
-  </div>
+          <li>
+            <a href="">질의응답</a>
+          </li>
+          <li>
+            <a href="">로그인</a>
+          </li>
+          <li>
+            <a href="">회원가입</a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 }
