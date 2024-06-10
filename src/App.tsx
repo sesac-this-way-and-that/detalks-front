64934515cf17f7577e2c3edb20a84a6ca0cb8d53
yzlybe <yzlybe@gmail.com>
2024-06-10 14:39:57 +0900

    fix: App.tsx 충돌 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import { Route, Routes } from "react-router-dom";
-import LoginPage from "./components/UserAuth/LoginPage";
-import RegisterPage from "./components/UserAuth/RegisterPage";
-import FindPasswordPage from "./components/UserAuth/FindPasswordPage";
 import "./styles/index.scss";
 import "./styles/userAuth.scss";
 import Main from "./components/Main";
 import NotFound from "./components/NotFound";
 import Mypage from "./components/Mypage/Mypage";
+import LoginPage from "./components/UserAuth/LoginPage";
+import RegisterPage from "./components/UserAuth/RegisterPage";
+import FindPassword from "./components/UserAuth/FindPasswordPage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
+      <Header></Header>
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/findPassword" element={<FindPasswordPage />} />
+        <Route path="/findPassword" element={<FindPassword />} />
         <Route path="/mypage" element={<Mypage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <Footer></Footer>
     </>
   );
 }
