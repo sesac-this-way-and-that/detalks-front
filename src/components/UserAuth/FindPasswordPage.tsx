321245f5f9a21f965a8b4c51e91bf8a91bb8fd77
yzlybe <yzlybe@gmail.com>
2024-06-12 14:27:09 +0900

    feat: 메일 인증 기능 추가 - 가입 시 입력한 메일로 인증번호를 전송 후 유저가 입력한 인증번호와 일치 시 DB에 유저 정보 추가 - AccountInputForm 컴포넌트 각 페이지로 분리

--- src/components/UserAuth/FindPasswordPage.tsx
+++ src/components/UserAuth/FindPasswordPage.tsx
@@ -1,10 +1,40 @@
+import { Link } from "react-router-dom";
 import accountStore from "../../store/userStore";
-import AccountInputForm from "./AccountInputForm";
+import EmailInput from "./EmailInput";
+import axios from "axios";
+import VerificationInput from "./VerificationInput";
+import { useState } from "react";
 
 export default function FindPasswordPage() {
+  const { email } = accountStore();
+  const [beforeVerification, setBeforeVerification] = useState<boolean>(true);
+  const accessType = "findPw";
+  const accessText = "비밀번호 찾기";
+
+  const submitFunc = () => {};
   return (
     <section>
-      <AccountInputForm accessType="findPw" accessText="비밀번호 찾기" />
+      <article className="accountInputForm">
+        <div className="pageType">
+          <div className="logo"></div>
+          <p>{accessText}</p>
+        </div>
+        {beforeVerification ? (
+          <form className="inputContainer">
+            <EmailInput accessType={accessType} />
+            <button type="submit">{accessText}</button>
+          </form>
+        ) : (
+          <form onSubmit={submitFunc}>
+            <VerificationInput />
+          </form>
+        )}
+      </article>
+      <article>
+        <Link to="/register">회원가입</Link>
+        <span> | </span>
+        <Link to="/login">로그인</Link>
+      </article>
     </section>
   );
 }
--- src/components/UserAuth/LoginPage.tsx
+++ src/components/UserAuth/LoginPage.tsx
@@ -1,15 +1,61 @@
-import AccountInputForm from "./AccountInputForm";
 import SocialAccount from "./SocialAccount";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Logout } from "./userInterface";
+import EmailInput from "./EmailInput";
+import PwdInput from "./PwdInput";
+import accountStore from "../../store/userStore";
+import axios from "axios";
 
 export default function LoginPage() {
+  const { email, pwd, setEmail, setPwd } = accountStore();
+  const nav = useNavigate();
+  const accessType = "login";
+  const accessText = "로그인";
+
+  const submitFunc = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const url = `${process.env.REACT_APP_API_SERVER}/member/signin`;
+    const userData = {
+      email: email,
+      pwd: pwd,
+    };
+    console.log("userdata: ", userData);
+    const formData = new FormData();
+    formData.append("email", email);
+    formData.append("pwd", pwd);
+    setEmail("");
+    setPwd("");
+    axios
+      .post(url, formData)
+      .then((res) => {
+        console.log("then res.data: ", res.data);
+        localStorage.setItem("authToken", res.data.token);
+        alert("로그인 성공");
+        nav("/");
+      })
+      .catch((err) => {
+        console.log("err: ", err);
+        alert("로그인 실패");
+      });
+  };
   return (
     <section>
-      <AccountInputForm accessType="login" accessText="로그인" />
-      <SocialAccount accessText="로그인" />
+      <article className="accountInputForm">
+        <div className="pageType">
+          <div className="logo"></div>
+          <p>{accessText}</p>
+        </div>
+        <form onSubmit={submitFunc} className="inputContainer">
+          <EmailInput accessType={accessType} />
+          <PwdInput accessType={accessType} />
+          <button type="submit">{accessText}</button>
+        </form>
+      </article>
+      <SocialAccount accessText={accessText} />
       <article>
         <Link to="/register">회원가입</Link>
+        <span> | </span>
         <Link to="/findPassword">비밀번호 찾기</Link>
       </article>
       <button onClick={Logout}>로그아웃 체크</button>
