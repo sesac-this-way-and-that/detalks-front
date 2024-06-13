a75aa09b4fd306a08bf862ca30e9296dc1891024
leekihyeok <789rlgur@naver.com>
2024-06-13 15:59:21 +0900

    Feat: Header 검색기능 중, 모바일 버전 완, style완

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,27 +3,60 @@ import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useInfoStore } from "../store";
 
 export default function Header() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  const userData = useInfoStore((state) => state.userInfo);
+  const getInfo = useInfoStore((state) => state.getInfo);
+  // const history = useHistory();
 
   useEffect(() => {
-    // Check for token in localStorage (or any other method you use for storing the token)
     const token = localStorage.getItem("authToken");
     if (token) {
+      getInfo();
       setIsAuthenticated(true);
     }
-  }, []);
+  }, [getInfo]);
 
   const handleLogout = () => {
-    // Remove token from localStorage (or any other method you use for storing the token)
     localStorage.removeItem("authToken");
     setIsAuthenticated(false);
   };
 
+  const handleSearchInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const handleSearch = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await axios.get(
+        `http://localhost:8080/api/questions/search`,
+        {
+          params: {
+            content: searchQuery,
+            sortBy: "voteCount",
+            page: 0,
+            size: 10,
+          },
+        }
+      );
+      console.log("Search Results:", response.data);
+      // history.push({
+      //   pathname: "/search-results",
+      //   state: { results: response.data },
+      // });
+    } catch (error) {
+      console.error("Search Error:", error);
+    }
+  };
+
   return (
     <section className="headerWrapper">
-      <div className="headerContainer">
+      <div className="headerContainer container">
         <div className="headerLogoContainer">
           <Link to="/">
             <img
@@ -39,20 +72,63 @@ export default function Header() {
           <input type="checkbox" className="toggle-menu" />
           <div className="hamburger"></div>
 
-          <div className="searchInput">
-            <input type="text" />
-            <button>
+          <form className="searchInput" onSubmit={handleSearch}>
+            <input
+              type="text"
+              value={searchQuery}
+              onChange={handleSearchInputChange}
+            />
+            <button type="submit">
               <FontAwesomeIcon icon={faMagnifyingGlass} />
             </button>
-          </div>
+          </form>
           <ul className="headerMenu">
+            {isAuthenticated ? (
+              <li className="myname-mobile">
+                <Link to="/mypage">
+                  <div className="profile-img">
+                    <img
+                      src={
+                        process.env.REACT_APP_STATIC_SERVER +
+                        "/" +
+                        userData?.img
+                      }
+                      alt={
+                        process.env.REACT_APP_STATIC_SERVER +
+                        "/" +
+                        userData?.img
+                      }
+                    />
+                  </div>
+                  <span>{userData?.name} 님</span>
+                </Link>
+              </li>
+            ) : (
+              <></>
+            )}
             <li>
               <Link to="/board">질의응답</Link>
             </li>
             {isAuthenticated ? (
               <>
-                <li>
-                  <Link to="/mypage">마이페이지</Link>
+                <li className="myname">
+                  <Link to="/mypage">
+                    <div className="profile-img">
+                      <img
+                        src={
+                          process.env.REACT_APP_STATIC_SERVER +
+                          "/" +
+                          userData?.img
+                        }
+                        alt={
+                          process.env.REACT_APP_STATIC_SERVER +
+                          "/" +
+                          userData?.img
+                        }
+                      />
+                    </div>
+                    <span>{userData?.name}</span> 님
+                  </Link>
                 </li>
                 <li>
                   <a href="#" onClick={handleLogout}>
