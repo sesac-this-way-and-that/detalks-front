f8f0fa0c7ad75f6fa72595552dbaf0547ee06110
sabb12 <sabb0923@gmail.com>
2024-06-14 11:54:18 +0900

    feat: discussion type

---
+++ src/types/discussion.ts
@@ -0,0 +1,37 @@
+export interface DiscussionInformation {
+  answerList: any[];
+  author: {
+    memberIdx: number;
+    memberName: string;
+  };
+  bookmarkState: boolean;
+  createdAt: Date;
+  isSolved: boolean;
+  modifiedAt: Date;
+  questionContent: string;
+  questionId: number;
+  questionState: boolean;
+  questionTitle: string;
+  tagNameList: string[];
+  viewCount: number;
+  voteCount: number;
+}
+
+export interface DiscussionDetail {
+  questionId: number;
+  questionTitle: string;
+  questionContent: string;
+  createdAt: Date;
+  modifiedAt: Date;
+  viewCount: number;
+  voteCount: number;
+  questionState: boolean;
+  isSolved: boolean;
+  author: {
+    memberIdx: number;
+    memberName: string;
+  };
+  answerList: string[];
+  tagNameList: string[];
+  bookmarkState: boolean;
+}
